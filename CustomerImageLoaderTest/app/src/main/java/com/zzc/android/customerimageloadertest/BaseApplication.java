b3acfa510dffe9a78839d84066f96ad0322fad2c
joye <631965991@qq.com>
2016-01-25 15:44:39 +0800

    init

---
+++ CustomerImageLoaderTest/app/src/main/java/com/zzc/android/customerimageloadertest/BaseApplication.java
@@ -0,0 +1,9 @@
+package com.zzc.android.customerimageloadertest;
+
+import android.app.Application;
+
+/**
+ * Created by zczhang on 16/1/25.
+ */
+public class BaseApplication extends Application{
+}
---
+++ CustomerImageLoaderTest/app/src/main/java/com/zzc/android/customerimageloadertest/SquareImageView.java
@@ -0,0 +1,34 @@
+package com.zzc.android.customerimageloadertest;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by zczhang on 16/1/24.
+ */
+public class SquareImageView extends ImageView{
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
